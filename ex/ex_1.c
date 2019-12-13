8810643ced06515f3b127cbc45c9276fd6cfe853
John Crow <johna.crow@corteva.com>
2019-12-13 14:00:46 -0600

    Does a few inserts and exists.

--- ex/ex_1.c
+++ ex/ex_1.c
@@ -3,7 +3,21 @@
 #include "hashtab.h"
 
 int
-main(int argc, char *argv[])
+main(void)
 {
+   struct hashtab *z = hashtab_new();
+
+   hashtab_insert(z, "dog", "woof");
+   hashtab_insert(z, "cat", "meow");
+   hashtab_insert(z, "pig", "oink");
+   hashtab_insert(z, "ant", "silence");
+
+   printf("%s says %s\n", "dog", (char *) hashtab_exists(z, "dog"));
+   printf("%s says %s\n", "cat", (char *) hashtab_exists(z, "cat"));
+   printf("%s says %s\n", "pig", (char *) hashtab_exists(z, "pig"));
+   printf("%s says %s\n", "ant", (char *) hashtab_exists(z, "ant"));
+
+   hashtab_free(&z);
+
    return 0;
 }
